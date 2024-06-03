72d885c820efc590128db9325f547b59ed3d54c2
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-06-03 09:43:49 -0300

    Adcionando componentes no bextjs

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import style from "../styles/Home.module.css"
 
 export default function Inicio() {
     return (
-        <div className={style.description}>
+        <div className={style.grid}>
             <h1>React e Next.js</h1>
             <h2>Vamos aprender novo framework</h2>
         </div>
